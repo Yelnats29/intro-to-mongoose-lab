7fb7a466f70117f0dea128fbac1fab6a1078727a
Yelnats <s.jeanjr@gmail.com>
2024-04-24 18:13:14 -0400

    updated functions. Need to correct if statements

--- app.js
+++ app.js
@@ -3,49 +3,94 @@ const dotenv = require('dotenv');
 dotenv.config();
 const mongoose = require('mongoose');
 const Customer = require('./customer.js');
+let customerName;
+let customerAge;
 
 
+//-----------------------------------------------------------//
 
+
+// Connect to Mongo
 const connect = async () => {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('Connected to MongoDB');
-    await runQueries()
+};
+
+// Disconnect from Mongo
+const disconnect = async () => {
     await mongoose.disconnect();
     console.log('Disconnected from MongoDB');
     process.exit();
 };
 
+// Create a new Customer
 const createCustomer = async () => {
     const customerData = {
-        name: `${individual}`,
-        age: `${age}`,
+        name: individual,
+        age: age,
     };
     const customer = await Customer.create(customerData);
     console.log("New Customer", customer)
 };
 
+// View all Customers
+const viewAllCustomers = async () => {
+    const customers = await Customer.find({})
+    console.log("All Customers:", customers);
+};
 
-const customerQueries = async () => {
+// Update a customer
+const updateCustomer = async () => {
+    const id = ""
+    const customer = await Customer.findByIdAndUpdate(id, { isComplete: true },
+        { new: true });
+    console.log("The New Updates:", customer)
+};
+
+// Delete a Customer
+const deleteCustomer = async () => {
+    const id = ""
+    const customer = await Customer.findByIdAndDelete(id);
+    console.log("Customer Has Been Deleted:", customer)
+};
+
+//Launch new customer function
+const createCustomerQueries = async () => {
     console.log(`Queries running.`);
     await createCustomer();
-}
+};
+
+//Launch View All Customers function
+const viewAllCustomersQueries = async () => {
+    console.log(`Queries running.`);
+    await viewAllCustomers();
+};
+
+//Launch Update a Customer function
+const updateCustomerQueries = async () => {
+    console.log(`Queries running.`);
+    await updateCustomer();
+};
+
+//Launch Delete a Customer function
+const deleteCustomerQueries = async () => {
+    console.log(`Queries running.`);
+    await deleteCustomer();
+};
+
+//Launch Quit/Disconnect function
+const disconnectQueries = async () => {
+    console.log(`Queries running.`);
+    await disconnect();
+};
 
 
 
+//--------------------------------------------------//
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+// Introduction to CRM
 const showIntro = () => {
     console.clear();
     const introMessage = `Welcome to the CRM. It's a pleasure to be able to assit you today!
@@ -64,11 +109,17 @@ const userIntroduction = () => {
     const username = prompt('What shall I call you? ');
 
     console.clear();
-    const greeting = prompt(`Welcome, ${username}, Please enjoy you're time today`);
+    const greeting = prompt(`Welcome, ${username}, Please enjoy your time today`);
 };
 
 userIntroduction();
 
+
+
+//-------------------------------------------------------------//
+
+
+// CRM Actions
 const action = () => {
     const choices = `What would you like to do?
 
@@ -76,36 +127,31 @@ const action = () => {
  2. View all customers \n
  3. Update a customer \n
  4. Delete a customer \n
- 5. quit \n
- `
+ 5. quit \n`
+
     console.log(choices)
-    const answer = prompt(`Please select a choice `);
+    const answer = prompt(`Please select a choice `).toLowerCase();
     console.clear();
 
 
-    if (choices.toLowerCase() === 1 || "create a customer" || `1. create a customer`) {
-        console.clear(); const individual = prompt("Enter the indiviual's name "); console.log(); const age = prompt("Enter the indiviual's age "); customerQueries();
-    } else if (action.toLowerCase() === "select a bot") {
-        console.clear(), selectBot();
+    if (answer == 1 || answer === "create a customer" || answer === `1. create a customer`) {
+        console.clear(); customerName = prompt("Enter the indiviual's name: "); console.log(); customerAge = prompt("Enter the indiviual's age: "); createCustomerQueries();
+    } else if (answer == 2 || answer === "view all customers" || `2. View all customers`) {
+        console.clear(), viewAllCustomersQueries();
+    } else if (answer == 3 || answer === "update a customer" || answer ===`3. Update a customer`) {
+        console.clear(), updateCustomerQueries();
+    } else if (answer == 4 || answer === "delete a customer" || answer === `4. Delete a customer`) {
+        console.clear(), deleteCustomerQueries();
+    } else if (answer == 5 || answer === "quit" || answer === `5. Quit`) {
+        console.clear(), disconnectQueries();
     } else {
-        console.log('Please give your Bot a valid command.');
+        console.log('Please select an answer choice from the above.');
         console.log();
-        getActions()
+        action()
     };
 }
 
 action();
-
-
-
-
-
-
-
-
-
-
-
 
 
 
